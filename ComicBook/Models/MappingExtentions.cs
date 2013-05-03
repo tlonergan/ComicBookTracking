4cfa290f579c902326b51f2f2f3ba9253ead3448
Travis Lonergan <travislonergan@gmail.com>
2013-05-02 23:11:10 -0700

    Initial use of Comic Vine

--- ComicBook/Models/MappingExtentions.cs
+++ ComicBook/Models/MappingExtentions.cs
@@ -1,87 +1,113 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ComicBook.Models
 {
     public static class MappingExtentions
     {
+        public static DateTime NextWednesday(this DateTime startDate)
+        {
+            IEnumerable<int> dayRange = Enumerable.Range(0, 7);
+            foreach (int day in dayRange)
+            {
+                DateTime testDate = startDate.AddDays(day);
+                if (testDate.DayOfWeek == DayOfWeek.Wednesday)
+                    return testDate;
+            }
+
+            throw new Exception("There are no more wednesdays... Apocolypse!");
+        }
+
+        public static DateTime PreviousWednesday(this DateTime startDate)
+        {
+            IEnumerable<int> dayRange = Enumerable.Range(0, 7);
+            foreach (int day in dayRange)
+            {
+                DateTime testDate = startDate.AddDays(day*-1);
+                if (testDate.DayOfWeek == DayOfWeek.Wednesday)
+                    return testDate;
+            }
+
+            throw new Exception("There were no wednesdays... Apocolypse!");
+        }
+
         public static Book MapBook(this ComicBook.Book book)
         {
             BookInventory inventory = book.BookInventories.FirstOrDefault() ?? new BookInventory
-                                                                               {
-                                                                                   InventoryStatusId = (int) InventoryStatus.Unread,
-                                                                                   InventoryStatu = new InventoryStatu
-                                                                                                    {
-                                                                                                        Id = (int) InventoryStatus.Unread,
-                                                                                                        Status = InventoryStatus.Unread.ToString()
-                                                                                                    }
-                                                                               };
+                {
+                    InventoryStatusId = (int) InventoryStatus.Unread,
+                    InventoryStatu = new InventoryStatu
+                        {
+                            Id = (int) InventoryStatus.Unread,
+                            Status = InventoryStatus.Unread.ToString()
+                        }
+                };
 
             return new Book
-                   {
-                       Id = book.Id,
-                       IsSpecialCover = book.IsSpecialCover,
-                       Issue = book.Issue,
-                       Notes = book.Notes,
-                       Status = (InventoryStatus) inventory.InventoryStatusId,
-                       StatusDisplay = inventory.InventoryStatu.Status,
-                       Series = book.Series.MapSeries()
-                   };
+                {
+                    Id = book.Id,
+                    IsSpecialCover = book.IsSpecialCover,
+                    Issue = book.Issue,
+                    Notes = book.Notes,
+                    Status = (InventoryStatus) inventory.InventoryStatusId,
+                    StatusDisplay = inventory.InventoryStatu.Status,
+                    Series = book.Series.MapSeries()
+                };
         }
 
         public static ComicBook.Book MapBook(this Book book)
         {
             return new ComicBook.Book
-                   {
-                       Id = book.Id,
-                       IsSpecialCover = book.IsSpecialCover,
-                       Issue = book.Issue,
-                       Notes = book.Notes,
-                       SeriesId = book.Series.Id,
-                       //BookInventories = new Collection<BookInventory>{new BookInventory{InventoryStatusId = (int)book.Status}},
-                       //WantLists = new Collection<WantList>{new WantList{WantListStatusId = (int)book.Want.StatusId}}
-                   };
+                {
+                    Id = book.Id,
+                    IsSpecialCover = book.IsSpecialCover,
+                    Issue = book.Issue,
+                    Notes = book.Notes,
+                    SeriesId = book.Series.Id,
+                    //BookInventories = new Collection<BookInventory>{new BookInventory{InventoryStatusId = (int)book.Status}},
+                    //WantLists = new Collection<WantList>{new WantList{WantListStatusId = (int)book.Want.StatusId}}
+                };
         }
 
         public static Series MapSeries(this ComicBook.Series series)
         {
             return new Series
-                   {
-                       Id = series.Id,
-                       IsCurrent = series.IsCurrent,
-                       Name = series.Name,
-                       Publisher = series.Publisher.MapPublisher()
-                   };
+                {
+                    Id = series.Id,
+                    IsCurrent = series.IsCurrent,
+                    Name = series.Name,
+                    Publisher = series.Publisher.MapPublisher()
+                };
         }
 
         public static Publisher MapPublisher(this ComicBook.Publisher publisher)
         {
             return new Publisher
-                   {
-                       Id = publisher.Id,
-                       Name = publisher.Name
-                   };
+                {
+                    Id = publisher.Id,
+                    Name = publisher.Name
+                };
         }
 
         public static ComicBook.Series MapSeries(this Series series)
         {
             return new ComicBook.Series
-                   {
-                       Id = series.Id,
-                       Name = series.Name,
-                       IsCurrent = series.IsCurrent,
-                       PublisherId = series.Publisher.Id
-                   };
+                {
+                    Id = series.Id,
+                    Name = series.Name,
+                    IsCurrent = series.IsCurrent,
+                    PublisherId = series.Publisher.Id
+                };
         }
 
         public static WantList MapWant(this Want want)
         {
             return new WantList
-                   {
-                       BookId = want.Book.Id,
-                       WantListStatusId = want.StatusId
-                   };
+                {
+                    BookId = want.Book.Id,
+                    WantListStatusId = want.StatusId
+                };
         }
 
         public static Want MapWant(this WantList wantListItem)
@@ -90,11 +116,11 @@ namespace ComicBook.Models
                 return null;
 
             return new Want
-                   {
-                       Id = wantListItem.Id,
-                       Status = (WantStatus) wantListItem.WantListStatusId,
-                       StatusDisplay = wantListItem.WantListStatu.Status
-                   };
+                {
+                    Id = wantListItem.Id,
+                    Status = (WantStatus) wantListItem.WantListStatusId,
+                    StatusDisplay = wantListItem.WantListStatu.Status
+                };
         }
 
         public static Location MapLocation(this ComicBook.Location location)
@@ -103,10 +129,10 @@ namespace ComicBook.Models
                 return null;
 
             return new Location
-                   {
-                       Id = location.Id,
-                       Name = location.Name
-                   };
+                {
+                    Id = location.Id,
+                    Name = location.Name
+                };
         }
 
         public static List<Location> MapLocations(this List<ComicBook.Location> dLocations)
